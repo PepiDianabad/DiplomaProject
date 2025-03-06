5f7d4fd2038288fb4830ede335c165269f48146c
PepiDianabad <petar.y.petrov@elsys-bg.org>
2025-03-06 12:52:32 +0200

    slight update in data.tf

--- DiplomnaRabota/terraform/modules/prometheus/data.tf
+++ DiplomnaRabota/terraform/modules/prometheus/data.tf
@@ -1,14 +1,9 @@
 data "aws_eks_cluster" "example" {
   name = "interview-prep-eks-cluster"  # EKS cluster name
 }
-
 data "aws_eks_cluster_auth" "example" {
   name = "interview-prep-eks-cluster"  # EKS cluster name
 }
-
-# data "aws_vpc" "example" {
-#   id = "vpc-0076a3641ca670234"
-# }
 # Fetch subnets dynamically based on the VPC
 data "aws_subnets" "example" {
   filter {
